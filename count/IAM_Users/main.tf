a0da3e398541e710950c98571154dcc8ced1e741
snvibes9 <snvibes9@gmail.com>
2025-08-16 23:12:01 +0530

    updated permissoin to the users

--- count/IAM_Users/main.tf
+++ count/IAM_Users/main.tf
@@ -13,3 +13,23 @@ resource "aws_iam_access_key" "user_keys" {
 
 }
 
+# Attach AdministratorAccess
+resource "aws_iam_user_policy_attachment" "admin_policy" {
+  count      = length(var.iam_user_names)
+  user       = aws_iam_user.users[count.index].name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+# Attach CloudWatch ReadOnly Access
+resource "aws_iam_user_policy_attachment" "cloudwatch_readonly" {
+  count      = length(var.iam_user_names)
+  user       = aws_iam_user.users[count.index].name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchReadOnlyAccess"
+}
+
+# Attach S3 ReadOnly Access
+resource "aws_iam_user_policy_attachment" "s3_readonly" {
+  count      = length(var.iam_user_names)
+  user       = aws_iam_user.users[count.index].name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+}
